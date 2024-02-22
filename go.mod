ea1c461d2ffb33c59f837c845ff00dfd7ded0418
INTLSavio <savio.leite@gec.inatel.br>
2024-02-22 17:22:15 -0300

    start project and entities

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/INTLSavio/auth-service
+
+go 1.21.5
+
+require (
+	github.com/google/uuid v1.6.0
+	golang.org/x/crypto v0.19.0
+)
---
+++ internal/entities/user.go
@@ -0,0 +1,30 @@
+package entities
+
+import (
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID string `json:"id"`
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+	Password string `json:"password"`
+}
+
+func NewUser(name, email, phone, password string) (*User, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password),
+		bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		ID: uuid.New().String(),
+		Name: name,
+		Email: email,
+		Phone: phone,
+		Password: string(hash),
+	}, nil
+}
